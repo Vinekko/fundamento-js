d0e2998f0d7e2132b0859c771a35a91746a6e084
Vincenzo Trigila <nitomaco@gmail.com>
2025-11-22 17:11:28 -0400

    conditionals in js

---
+++ js/04conditionals.js
@@ -0,0 +1,15 @@
+// conditionals en javascript
+
+// evaluacion de edad para adulto mayor
+
+const age = -10;
+if (age >= 65) {
+    console.log("You are old enough to retire.");
+} else if (age >= 18) {
+    console.log("You are old enough to vote.");
+} else if (age < 0) {
+    console.log("Age cannot be negative.");
+} else {
+    console.log("You are not old enough to vote.");
+}
+
