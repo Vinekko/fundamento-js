51deddce755fddd9f2591b357790d327f15137bc
Vincenzo Trigila <nitomaco@gmail.com>
2025-11-22 16:25:12 -0400

    first commit

---
+++ js/01Variables.js
@@ -0,0 +1,11 @@
+// Declaracion de variables
+
+const name = "Juan"; // Declaracion de variables que son constantes y que no pueden ser modificadas
+let age = 25; // Declaracion de variables que pueden ser modificadas
+const lastName = "Perez"; // Declaracion de variables que pueden ser modificadas
+let genre = "Masculino"; // Declaracion de variables que pueden ser modificadas
+let isStudent = true; // Declaracion de variables que pueden ser modificadas booleanas
+let price = 19.99; // Declaracion de variables que pueden ser modificadas 
+
+// mostrar variables en la consola
+alert(name);
