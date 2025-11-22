49f23827e4010c50c6bb07c99f21db20867b3033
Vincenzo Trigila <nitomaco@gmail.com>
2025-11-22 16:55:29 -0400

    objects in js

---
+++ js/03object.js
@@ -0,0 +1,16 @@
+// objects en javascript
+// lo que determina el objeto es la clave y el valor
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York",
+    address: {
+        street: "123 Main St",
+        city: "New York",
+        state: "NY",
+        zip: "10001"
+    },
+    hobbies: ["reading", "traveling", "sports"],
+    isStudent: false
+};
+console.log(person);
