4902f76c86bb385363269b18a31ffc176eb4bbe3
Vincenzo Trigila <nitomaco@gmail.com>
2025-11-22 17:19:53 -0400

    ForLoops in js

---
+++ js/05ForLoops.js
@@ -0,0 +1,12 @@
+// for loops en javascript
+
+// bucle for
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//recorrer un array
+const fruits = ["apple", "banana", "cherry"];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
